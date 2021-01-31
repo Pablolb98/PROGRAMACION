971a440ed95ccbb0208dd59349dd8d4a798d8952
Pablolb98 <72375933+Pablolb98@users.noreply.github.com>
2021-01-31 19:47:53 +0100

    Las buenas mates

---
+++ Ejercicios/src/capitulo06_Recursos/Math/EcuacionSegundoGrado.java
@@ -0,0 +1,37 @@
+package capitulo06_Recursos.Math;
+
+import javax.swing.JOptionPane;
+
+public class EcuacionSegundoGrado {
+
+	public static void main(String[] args) {
+		
+		int a, b, c;
+		double formula1, formula2;
+		String str;
+		
+		str = JOptionPane.showInputDialog("Introduce el parámetro A:");
+		a = Integer.parseInt(str); //1
+		
+		str = JOptionPane.showInputDialog("Introduce el parámetro B:");
+		b = Integer.parseInt(str); //3
+		
+		str = JOptionPane.showInputDialog("Introduce el parámetro C:");
+		c = Integer.parseInt(str); //2
+		
+		double raiz;
+		double denominador;
+		
+		denominador = 2 * a;
+		
+		raiz = Math.sqrt((Math.pow(b, 2))-4*a*c);
+		
+		
+		formula1 = (-b + raiz ) / denominador;
+		formula2 = (-b - raiz ) / denominador;
+		
+		
+		System.out.println("El resultado es: " + formula1 + " y por el negativo: " + formula2);
+	}
+
+}
